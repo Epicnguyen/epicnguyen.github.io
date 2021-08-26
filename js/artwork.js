e3405f0de059887e01a804d8c4dc9c096f423478
Epicnguyen <toan.nguyen.productions@gmail.com>
2021-08-26 19:50:21 -0400

    added mobile nav

--- js/artwork.js
+++ js/artwork.js
@@ -5,6 +5,7 @@
   // Get the image and insert it inside the modal - use its "alt" text as a caption
   var modalImg = document.getElementById("img01");
   var captionText = document.getElementById("caption");
+  var nav = document.getElementById("nav");
 
   function zoom(img, alt){
     modal.style.display = "block";
@@ -13,10 +14,15 @@
   }
 
   // Get the <span> element that closes the modal
-  var span = document.getElementsByClassName("close")[0];
+  var menu = document.getElementsByClassName("close")[0];
+  var span = document.getElementsByClassName("close")[1];
 
   // When the user clicks on <span> (x), close the modal
   span.onclick = function() {
     modal.style.display = "none";
     document.img = "";
+  }
+
+  menu.onclick = function() {
+    nav.style.display = "none";
   }
